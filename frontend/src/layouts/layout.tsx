d379425082785b592823c898d1b805c786539802
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-11 19:27:46 +0200

    Added Home page

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 type Props = {
     children: React.ReactNode;
@@ -9,7 +10,8 @@ type Props = {
 const Layout = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
-            <Header/>
+            <Header />
+            <Hero />
             <div className="container mx-auto flex-1 py-10">{children}</div>
         </div>
     );
