4a55213f5a04f3e092f61e616da8249b923c826c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-11 18:37:08 +0200

    Added Mobile and Main navigation component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import MobileNav from './MobileNav';
+import MainNav from './MainNav';
 
 const Header = () => {
     return (
@@ -11,10 +13,10 @@ const Header = () => {
                     eRaEats.com
                 </Link>
                 <div className="md:hidden">
-                    {/* <MobileNav /> */}
+                    <MobileNav />
                 </div>
                 <div className="hidden md:block">
-                    {/* <MainNav /> */}
+                    <MainNav />
                 </div>
             </div>
         </div>
