55c25119b704fdfdd742413bafda0ab7ef0c3675
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-11 20:04:06 +0200

    Added Footer component

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import Footer from '@/components/Footer';
 
 type Props = {
     children: React.ReactNode;
@@ -13,6 +14,7 @@ const Layout = ({ children }: Props) => {
             <Header />
             <Hero />
             <div className="container mx-auto flex-1 py-10">{children}</div>
+            <Footer />
         </div>
     );
 };
