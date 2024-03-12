1c6fb8c23b742ed1bddecdb8c391bd301c37be3a
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-12 22:18:07 +0200

    Added 'useCreateMyUser' api

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import './global.css';
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false
+        }
+    }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Router>
-            <Auth0ProviderWithNavigate>
-                <AppRoutes />
-            </Auth0ProviderWithNavigate>
+            <QueryClientProvider client={queryClient}>
+                <Auth0ProviderWithNavigate>
+                    <AppRoutes />
+                </Auth0ProviderWithNavigate>
+            </QueryClientProvider>
         </Router>
     </React.StrictMode>,
 );
