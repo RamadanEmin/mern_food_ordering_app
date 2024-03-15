3277fc809eecccbf68e7b2e25b02ee530ae1a1ba
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-15 17:58:13 +0200

    Added validations for restorant request

--- backend/src/routes/MyRestaurantRoure.ts
+++ backend/src/routes/MyRestaurantRoure.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import { jwtCheck, jwtParse } from '../middleware/auth';
 import MyRestaurantController from '../controllers/MyRestaurantController';
+import { validateMyRestaurantRequest } from '../middleware/validation';
 
 const router = express.Router();
 
@@ -16,6 +17,7 @@ const upload = multer({
 router.post(
     '/',
     upload.single('imageFile'),
+    validateMyRestaurantRequest,
     jwtCheck,
     jwtParse,
     MyRestaurantController.createMyRestaurant
