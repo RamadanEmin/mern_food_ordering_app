f7dd88d2f23b56f382c791cf6ecc1ddafd611ede
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-15 16:13:24 +0200

    Added Restaurant model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/my/user', myUserRoute); 
+app.use('/health', async (req: Request, res: Response) => {
+    res.send({ message: 'health ok!!!' });
+});
+
+app.use('/api/my/user', myUserRoute);
 
 app.listen(7000, () => {
     console.log('Server started on localhost:7000');
