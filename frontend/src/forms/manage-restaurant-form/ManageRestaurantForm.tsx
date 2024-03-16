1c5cdfa6c535438e686b2a39dc7431e1fa95d4da
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-16 12:17:52 +0200

    Added Cuisines section component

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import DetailsSection from './DetailsSection';
 import { Separator } from '@/components/ui/separator';
+import CuisinesSection from './CuisinesSection';
 
 const formSchema = z.object({
     restaurantName: z.string({
@@ -59,7 +60,8 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
                 <DetailsSection />
-                <Separator/>
+                <Separator />
+                <CuisinesSection />
             </form>
         </Form>
     );
