857cf097c24255d9539c121734b65af25169f545
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-17 17:12:25 +0200

    Update Image section

--- frontend/src/forms/manage-restaurant-form/ImageSection.tsx
+++ frontend/src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,9 +1,12 @@
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useFormContext } from 'react-hook-form';
 
 const ImageSection = () => {
-    const { control } = useFormContext();
+    const { control, watch } = useFormContext();
+
+    const existingImageUrl = watch('imageUrl');
 
     return (
         <div className="space-y-2">
@@ -15,7 +18,16 @@ const ImageSection = () => {
                 </FormDescription>
             </div>
 
-            <div className="flex flex-col gap-8 w-[50%]">
+            <div className="flex flex-col gap-8 md:w-[50%]">
+                {existingImageUrl && (
+                    <AspectRatio ratio={16 / 9}>
+                        <img
+                            src={existingImageUrl}
+                            alt="image"
+                            className='rounded-md object-cover h-full w-full'
+                        />
+                    </AspectRatio>
+                )}
                 <FormField
                     control={control}
                     name="imageFile"
@@ -29,7 +41,7 @@ const ImageSection = () => {
                                     onChange={(event) => field.onChange(event.target.files ? event.target.files[0] : null)}
                                 />
                             </FormControl>
-                            <FormMessage/>
+                            <FormMessage />
                         </FormItem>
                     )}
                 />
