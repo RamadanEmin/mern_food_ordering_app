961e2701a0b05f64dfeb2541c43568c571122c8c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-19 22:18:31 +0200

    Added additional search query

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,7 +1,8 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi';
-import SearchBar from '@/components/SearchBar';
+import SearchBar, { SearchForm } from '@/components/SearchBar';
 import SearchResultCard from '@/components/SearchResultCard';
 import SearchResultInfo from '@/components/SearchResultInfo';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export type SearchState = {
@@ -10,7 +11,24 @@ export type SearchState = {
 
 const SearchPage = () => {
     const { city } = useParams();
-    const { results, isLoading } = useSearchRestaurants(city);
+    const [searchState, setSearchState] = useState<SearchState>({
+        searchQuery: ''
+    });
+    const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+    const setSearchQuery = (searchFormData: SearchForm) => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: searchFormData.searchQuery
+        }));
+    };
+
+    const resetSearch = () => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: ''
+        }));
+    };
 
     if (isLoading) {
         <span>Loading...</span>;
@@ -26,7 +44,12 @@ const SearchPage = () => {
             </div>
 
             <div id="main-content" className="flex flex-col gap-5">
-                <SearchBar/>
+                <SearchBar
+                    searchQuery={searchState.searchQuery}
+                    onSubmit={setSearchQuery}
+                    placeHolder="Search by Cuisine or Restaurant Name"
+                    onReset={resetSearch}
+                />
                 <SearchResultInfo total={results.pagination.total} city={city} />
                 {results.data.map((restaurant) => (
                     <SearchResultCard restaurant={restaurant} />
