70c919bd3433b4b2e854a6a38a7f00fd0a23010f
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-23 13:00:31 +0200

    Create stripe session

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -2,6 +2,9 @@ import Stripe from 'stripe';
 import { Request, Response } from 'express';
 import Restaurant, { MenuItemType } from '../models/restaurant';
 
+const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
+const FRONTEND_URL = process.env.FRONTEND_URL as string;
+
 type CheckoutSessionRequest = {
     cartItems: {
         menuItemId: string;
@@ -31,6 +34,19 @@ const createCheckoutSession = async (req: Request, res: Response) => {
             checkoutSessionRequest,
             restaurant.menuItems
         );
+
+        const session = await createSession(
+            lineItems,
+            'TEST_ORDER_ID',
+            restaurant.deliveryPrice,
+            restaurant._id.toString()
+        );
+
+        if (!session.url) {
+            return res.status(500).json({ message: "Error creating stripe session" });
+        }
+
+        res.json({ url: session.url });
     } catch (error: any) {
         console.log(error);
         res.status(500).json({ message: error.raw.message });
@@ -63,6 +79,38 @@ const createLineItems = (
     });
 
     return lineItems;
+};
+
+const createSession = async (
+    lineItems: Stripe.Checkout.SessionCreateParams.LineItem[],
+    orderId: string,
+    deliveryPrice: number,
+    restaurantId: string
+) => {
+    const sessionData = await STRIPE.checkout.sessions.create({
+        line_items: lineItems,
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                    display_name: "Delivery",
+                    type: "fixed_amount",
+                    fixed_amount: {
+                        amount: deliveryPrice,
+                        currency: "eur"
+                    }
+                }
+            }
+        ],
+        mode: "payment",
+        metadata: {
+            orderId,
+            restaurantId
+        },
+        success_url: `${FRONTEND_URL}/order-status?success=true`,
+        cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+    });
+
+    return sessionData;
 };
 
 export default {
