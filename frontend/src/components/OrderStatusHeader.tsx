85015c78bd152befecc5f40680bc449362d26533
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-25 20:45:37 +0200

    Added Expected delivery function

--- frontend/src/components/OrderStatusHeader.tsx
+++ frontend/src/components/OrderStatusHeader.tsx
@@ -1,4 +1,5 @@
 import { Order } from '@/types';
+import { Progress } from './ui/progress';
 import { ORDER_STATUS } from '@/config/order-status-config';
 
 type Props = {
@@ -6,6 +7,19 @@ type Props = {
 };
 
 const OrderStatusHeader = ({ order }: Props) => {
+    const getExpectedDelivery = () => {
+        const created = new Date(order.createdAt);
+
+        created.setMinutes(created.getMinutes() + order.restaurant.estimatedDeliveryTime);
+
+        const hours = created.getHours();
+        const minutes = created.getMinutes();
+
+        const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+        return `${hours}:${paddedMinutes}`;
+    };
+
     const getOrderStatusInfo = () => {
         return (
             ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
@@ -16,7 +30,12 @@ const OrderStatusHeader = ({ order }: Props) => {
         <>
             <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
                 <span> Order Status: {getOrderStatusInfo().label}</span>
+                <span> Expected by: {getExpectedDelivery()}</span>
             </h1>
+            <Progress
+                className="animate-pulse"
+                value={getOrderStatusInfo().progressValue}
+            />
         </>
     );
 };
